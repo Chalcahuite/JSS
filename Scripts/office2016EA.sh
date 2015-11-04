aeec1b50c0a6be929c136894a9a94680ffa46df9
Chalcahuite <Chalcahuite@gmail.com>
2015-11-04 16:42:30 -0500

    Optimized by tulgeywood

--- Scripts/office2016EA.sh
+++ Scripts/office2016EA.sh
@@ -1,16 +1,10 @@
 #!/bin/bash
-count=0
 officeApps=( "Excel.app" "Outlook.app" "PowerPoint.app" "Word.app")
 for app in ${officeApps[*]}; do
-	if [[ -d "/Applications/Microsoft $app" ]]; then
-		count=$((count+1))
-		else
-		count=0
+	if ! [[ -d "/Applications/Microsoft $app" ]]; then
+		echo "<result>False</result>"
+		exit 0
 	fi
-	done
-echo "$count"
-if [[ ${count} -eq 4 ]]; then	
-	echo "<result>True</result>"
- 	else
-    echo "<result>False</result>"
-fi
+done	
+echo "<result>True</result>"
+exit 0
