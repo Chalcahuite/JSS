30d70cbdb6b03a83800ee4caa31c7ad6fd564c90
Sergio Aviles <Sergio_Aviles@cable.comcast.com>
2018-08-23 16:54:05 -0400

    Updated script

--- EAs/*EATemplateReadBoolFromPlist.py
+++ EAs/*EATemplateReadBoolFromPlist.py
@@ -1,17 +1,17 @@
 #!/usr/bin/env python
 
+"""EA Template for reading boolean value from a .plist."""
+
 import os
 import CoreFoundation
-
-''' EA Template for reading boolean value from a .plist '''
 
 plist = '/Library/Preferences/com.comcast.cable.cis.plist'
 
 if os.path.exists(plist):
-    ''' Substitute key (FirstBootRun) and bundle identifier (com.comcast.cable.cis) as necessary'''
-    value = CoreFoundation.CFPreferencesCopyAppValue("FirstBootRun", "com.comcast.cable.cis")
+    """Substitute key (FirstBootRun) and bundle identifier (com.comcast.cable.cis) as necessary."""
+    value = CoreFoundation.CFPreferencesCopyAppValue(
+        "FirstBootRun", "com.comcast.cable.cis")
     if value is None:
         print '<results>False</results>'
     else:
         print '<results>%s</results>' % value
-
